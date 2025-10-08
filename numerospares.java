7186067626aae22cd9960662571c9cbf537be58b
GabrielOrtega23 <ortegafaiagabriel@gmail.com>
2025-10-08 14:47:49 -0300

    Add files via upload

---
+++ numerospares.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class numerospares{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+       int[] numeros = new int[10];
+int[] pares = new int[10];
+int contadorPares = 0;
+
+System.out.println("Digite 100 números:");
+for (int i = 0; i < numeros.length; i++){
+    System.out.print("Número" + (i + 1) + ":");
+    numeros[i] = input.nextInt();
+
+    if(numeros[i] % 2 == 0) {
+        pares[contadorPares] = numeros[i];
+        contadorPares++;
+    }
+
+}
+System.out.println("\nNúmeros pares:");
+for (int i = 0; i < contadorPares; i++) {
+    System.out.println(pares[i]);
+}
+
+
+
+
+    }
+}
